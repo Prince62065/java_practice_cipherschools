89dce1e558df122fb37cb0850ee1b6d62c24ffe2
Demo <princekumar09372@gmail.com>
2025-02-03 13:42:39 +0530

    question4

---
+++ question4.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class question4 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age=sc.nextInt();
+        System.out.print("Enter your monthly income: ");
+        double income=sc.nextDouble();
+        System.out.print("Do you have pending dues (true/false): ");
+        boolean dues=sc.nextBoolean();
+        boolean eligibility=(age>=18 && income>=30000 && dues==false)?true:false;
+
+        if(eligibility==true){
+            System.out.println("You are eligible for premimum membership.");
+        }
+        else{
+            System.out.println("You are not eligible for premium membership.");
+        }
+        sc.close();
+    }
+}
+
